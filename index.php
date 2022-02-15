555fc80a814a31467b50a756efbc7cf2a936d48b
Mickaël AUGER <auger.mickael37@gmail.com>
2022-02-15 11:35:07 +0100

    mon permier commit

---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+declare(strict_types=1);
+
+$merlin    = [
+    'nom'        => 'Merlin',
+    'profession' => 'Druide',
+    'pv'         => 200,
+    'atk'        => 80
+];
+$arthur    = [
+    'nom'        => 'Arthur',
+    'profession' => 'Roi',
+    'pv'         => 400,
+    'atk'        => 60
+];
+$guenievre = [
+    'nom'        => 'Guenièvre',
+    'profession' => 'Reine',
+    'pv'         => 100,
+    'atk'        => 20
+];
+$perceval  = [
+    'nom'        => 'Perceval',
+    'profession' => 'Chevalier',
+    'pv'         => 300,
+    'atk'        => 40
+];
+$mordred   = [
+    'nom'        => 'Mordred',
+    'profession' => 'Chevalier',
+    'pv'         => 400,
+    'atk'        => 40
+];
+
+/*switch ($arthur['profession']) {
+    case 'Chevalier':
+        echo 'Sir ! On en a gros! <br>';
+        break;
+    case 'Druide':
+        echo 'Franchement, une potion pour faire pisser bleu, ça presse forcément la minute ? <br>';
+        break;
+    case 'Reine':
+        echo 'Ah d\'accord, vous me prenez vraiment, pour la reine des pécores. <br>';
+        break;
+    case 'Roi':
+        echo 'Rangez moi ça, débile, la Table Ronde, c\'est pas la fête de l\'artisanat ! <br>';
+        break;
+    default:
+        echo 'Où elle est la poulette ? <br>';
+}*/
+
+// Perceval attaque Mordred
+//while ($mordred['pv'] > 0) {
+//    $mordred['pv'] -= $perceval['atk'];
+//}
+
+
+/**
+ * @param array $attaquant
+ * @param array $cible
+ * @return int
+ */
+function attaquer(array $attaquant, array &$cible): int
+{
+    return $cible['pv'] -= $attaquant['atk'];
+}
+
+/**
+ * @param int $pv
+ * @return void
+ */
+function messagePV(int $pv): void
+{
+    if ($pv === 0) {
+        echo 'Vous avez toujours une mine de chiotte mais là... j\'sais pas si c\'est la lumière du feu ou quoi mais on dirait que vous êtes mort. <br>';
+    } elseif ($pv <= 50) {
+        echo 'Vous allez me faire le plaisir de faire péter cette saloperie que je puisse faire sortir la machine, hein! <br>';
+    } else {
+        echo 'Ce qu\'il y a de bien avec les opinions tranchées, c\'est qu\'ça relance le débat. En somme vous êtes une sorte de provocateur, quoi. <br>';
+    }
+}
+
+echo attaquer($perceval, $mordred);
+messagePV($mordred['pv']);
+
+echo attaquer($perceval, $mordred);
+messagePV($mordred['pv']);
+
+messagePV(attaquer($perceval, $mordred));
+
+
